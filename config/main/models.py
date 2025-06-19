ce9e05d8a95b0a8bd901f891071e067328a382c8
0MAVM0 <ediconpts9@gmail.com>
2025-06-19 18:04:51 +0500

    Model - Course

--- config/main/models.py
+++ config/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    img = models.ImageField(upload_to='courses/', null=True, blank=True)
+    description = models.TextField()
+    beginning = models.TimeField(null=False, blank=False)
+    duration = models.IntegerField(null=False, blank=False, default=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
+    # tutor = models.ForeignKey(Mentor, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
