ebf124b1029a5fb903ce817620a65fd64a6cfd4d
0MAVM0 <ediconpts9@gmail.com>
2025-06-19 18:13:26 +0500

    Changes In Model - Course

--- config/main/models.py
+++ config/main/models.py
@@ -6,9 +6,9 @@ class Course(models.Model):
     img = models.ImageField(upload_to='courses/', null=True, blank=True)
     description = models.TextField()
     beginning = models.TimeField(null=False, blank=False)
-    duration = models.IntegerField(null=False, blank=False, default=2)
+    duration = models.IntegerField(null=True, blank=True, default=2)
+    times_a_week = models.IntegerField(null=True, blank=True, default=3)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
-    # tutor = models.ForeignKey(Mentor, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.name
