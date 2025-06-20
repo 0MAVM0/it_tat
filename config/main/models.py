3e3c4971864b7e946f23e75cdfeb746842996269
0MAVM0 <ediconpts9@gmail.com>
2025-06-20 19:42:02 +0500

    Unnecessary Params

--- config/main/models.py
+++ config/main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Technology(models.Model):
-    name = models.CharField(max_length=255, null=False, blank=False)
+    name = models.CharField(max_length=255)
     img = models.ImageField(upload_to='technologies/', null=True, blank=True)
 
     def __str__(self) -> str:
@@ -10,15 +10,15 @@ class Technology(models.Model):
 
 
 class Course(models.Model):
-    name = models.CharField(max_length=255, null=False, blank=False)
+    name = models.CharField(max_length=255)
     img = models.ImageField(upload_to='courses/', null=True, blank=True)
     description = models.TextField()
     has_ai = models.BooleanField(default=False)
-    duration_of_course = models.CharField(max_length=15, null=False, blank=False)
-    beginning = models.TimeField(null=False, blank=False)
-    duration_of_a_lesson = models.CharField(max_length=15, null=False, blank=False)
-    times_a_week = models.CharField(max_length=15, null=False, blank=False)
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
+    duration_of_course = models.CharField(max_length=15)
+    beginning = models.TimeField()
+    duration_of_a_lesson = models.CharField(max_length=15)
+    times_a_week = models.CharField(max_length=15)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     technology = models.ForeignKey(Technology, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
@@ -26,20 +26,20 @@ class Course(models.Model):
 
 
 class Portfolio(models.Model):
-    title = models.CharField(max_length=50, null=False, blank=False)
+    title = models.CharField(max_length=50)
     description = models.TextField()
     img = models.ImageField(upload_to='portfolio/', null=True, blank=True)
     link = models.URLField(max_length=255, null=True, blank=True)
 
     def __str__(self) -> str:
-        return f'{self.title} - {self.mentor.full_name}'
+        return self.title
 
 
 class Mentor(models.Model):
-    full_name = models.CharField(max_length=255, null=False, blank=False)
+    full_name = models.CharField(max_length=255)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     img = models.ImageField(upload_to='mentors/', null=True, blank=True)
-    job_experience = models.IntegerField(null=False, blank=False)
+    job_experience = models.IntegerField()
     graduated_students = models.IntegerField(null=True,  blank=True)
     description = models.TextField()
     portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
@@ -50,7 +50,7 @@ class Mentor(models.Model):
 
 class Feedback(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=255, null=False, blank=False)
+    full_name = models.CharField(max_length=255)
     video = models.FileField(upload_to='feedbacks/', null=True, blank=True)
 
     def __str__(self) -> str:
@@ -58,26 +58,26 @@ class Feedback(models.Model):
 
 
 class LessonsVideo(models.Model):
-    title = models.CharField(max_length=255, null=False, blank=False)
+    title = models.CharField(max_length=255)
     tutor = models.ForeignKey(Mentor, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    video = models.FileField(upload_to='lessons_videos/', null=False, blank=False)
+    video = models.FileField(upload_to='lessons_videos/')
 
     def __str__(self) -> str:
         return self.title
 
 
 class ProgramRequest(models.Model):
-    full_name = models.CharField(max_length=150, null=False, blank=False)
-    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    full_name = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=20)
 
     def __str__(self) -> str:
         return self.full_name
 
 
 class FAQ(models.Model):
-    question = models.CharField(max_length=255, null=False, blank=False)
-    responce = models.TextField(null=False, blank=False)
+    question = models.CharField(max_length=255)
+    response = models.TextField()
 
     def __str__(self) -> str:
         return self.question
